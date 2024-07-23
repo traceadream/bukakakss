3b779c4e206cad9784abc664e817357abec3cef4
traceadream <achthoriqalamsyah@gmail.com>
2024-07-23 17:56:28 +0700

    main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
     const c = setTimeout(() => {
       document.body.classList.remove("not-loaded");
   
-      const titles = ('화이팅 아야 누나!!').split('')
+      const titles = ('기운내요 아야 누나!!').split('')
       const titleElement = document.getElementById('title');
       let index = 0;
   
