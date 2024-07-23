f0bbf6c329c540ab26bfd1d466ffc0c9954c6c55
traceadream <achthoriqalamsyah@gmail.com>
2024-07-23 17:04:35 +0700

    index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = 'Bisa Dibuka Tangan Suci'.split('')
+const text = 'Hanya Bisa Dibuka oleh Tangan yang Suci'.split('')
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
     title.innerHTML += `<span>${text[index]}<span/>`
